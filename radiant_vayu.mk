879be0f2f42c1be52a86e34c3f937f628bd04e4a
MLZ94 <zugutoroberto@gmail.com>
2021-08-28 23:09:47 +0200

    vayu: inherit common full phone

--- radiant_vayu.mk
+++ radiant_vayu.mk
@@ -12,7 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/vayu/device.mk)
 
 # Inherit common Project Radiant configurations
-$(call inherit-product, vendor/radiant/config/common.mk)
+$(call inherit-product, vendor/radiant/config/common_full_phone.mk)
 
 PRODUCT_NAME := radiant_vayu
 PRODUCT_DEVICE := vayu
